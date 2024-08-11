f42efd2149c72150ed018aca959cae6c4dd22131
CopyPaste <sjnnj@naver.com>
2024-08-11 19:42:33 +0900

    #18 - 패키지 명 업데이트
    업데이트

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,15 +1,10 @@
 package com.fastcampus.projectboard.repository;
 
-import com.fastcampus.projectboard.configs.JpaConfig;
+import com.fastcampus.projectboard.config.JpaConfig;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Import(JpaConfig.class)
