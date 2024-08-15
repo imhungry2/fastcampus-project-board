a6d39b524e9e80347674897090754d0126b2de04
CopyPaste <sjnnj@naver.com>
2024-08-15 20:14:33 +0900

    #22 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fastcampus/projectboard/domain/dto/ArticleDto.java
+++ src/main/java/com/fastcampus/projectboard/domain/dto/ArticleDto.java
@@ -1,15 +1,42 @@
 package com.fastcampus.projectboard.domain.dto;
 
+import com.fastcampus.projectboard.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
+        Long id,
+        UserAccountDto userAccountDto,
+        String hashtag,
         LocalDateTime createdAt,
         String createdBy,
-        String title,
-        String content,
-        String hashtag
+        LocalDateTime modifiedAt,
+        String modifiedBy
 ) {
-    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
-        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
     }
 }
