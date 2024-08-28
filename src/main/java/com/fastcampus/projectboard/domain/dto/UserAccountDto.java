272223a1b100f391cd76771ff475e33d1db32da0
CopyPaste <sjnnj@naver.com>
2024-08-28 15:18:59 +0900

    #44 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId` 로 id 를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/fastcampus/projectboard/domain/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboard/domain/dto/UserAccountDto.java
@@ -1,13 +1,10 @@
 package com.fastcampus.projectboard.domain.dto;
 
-
-
 import com.fastcampus.projectboard.domain.UserAccount;
 
 import java.time.LocalDateTime;
 
 public record UserAccountDto(
-        Long id,
         String userId,
         String userPassword,
         String email,
@@ -19,13 +16,13 @@ public record UserAccountDto(
         String modifiedBy
 ) {
 
-    public static UserAccountDto of(Long id, String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo,
+                                    LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
-                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 entity.getEmail(),
