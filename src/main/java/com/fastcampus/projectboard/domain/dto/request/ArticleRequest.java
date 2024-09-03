96f2051e8a992b7e79e2f978b670fa958139eac6
CopyPaste <sjnnj@naver.com>
2024-09-03 18:24:19 +0900

    #59 - 도메인 변경점의 반영: 해시태그
    해시태그 엔티티가 추가되고, 게시글이 해시태그를 여러 개
    가질 수 있도록 설계가 변경됨.
    이 때문에 변경점이 크게 전파되었다.
    커밋이 컴파일 에러 상태로 두는 것은 좋지 않아서
    어쩔 수 없이 변경점을 모두 한 커밋에 담는데
    이 방법이 괜찮을지는 추후 더 고민해보자.
    
    그러나 도메인 변경만 반영하고 기능을 구현하지 않았으므로
    컴파일 에러만 잠시 피할 뿐, 테스트는 현재 실패함

--- src/main/java/com/fastcampus/projectboard/domain/dto/request/ArticleRequest.java
+++ src/main/java/com/fastcampus/projectboard/domain/dto/request/ArticleRequest.java
@@ -1,24 +1,30 @@
 package com.fastcampus.projectboard.domain.dto.request;
 
 import com.fastcampus.projectboard.domain.dto.ArticleDto;
+import com.fastcampus.projectboard.domain.dto.HashtagDto;
 import com.fastcampus.projectboard.domain.dto.UserAccountDto;
+
+import java.util.Set;
 
 public record ArticleRequest(
         String title,
-        String content,
-        String hashtag
+        String content
 ) {
 
-    public static ArticleRequest of(String title, String content, String hashtag) {
-        return new ArticleRequest(title, content, hashtag);
+    public static ArticleRequest of(String title, String content) {
+        return new ArticleRequest(title, content);
     }
 
     public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return toDto(userAccountDto, null);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
         return ArticleDto.of(
                 userAccountDto,
                 title,
                 content,
-                hashtag
+                hashtagDtos
         );
     }
 }
