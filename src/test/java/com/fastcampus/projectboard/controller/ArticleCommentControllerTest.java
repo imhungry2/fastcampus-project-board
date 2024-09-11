9f2f7506e664ac4d2a5a1dd3ade3cd1d79c14e2a
CopyPaste <sjnnj@naver.com>
2024-09-11 14:29:16 +0900

    #75 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가
    대댓글이 포함된 댓글 저장 api 의 입출력 테스트 추가

--- src/test/java/com/fastcampus/projectboard/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleCommentControllerTest.java
@@ -34,7 +34,8 @@ class ArticleCommentControllerTest {
     private final MockMvc mvc;
     private final FormDataEncoder formDataEncoder;
 
-    @MockBean private ArticleCommentService articleCommentService;
+    @MockBean
+    private ArticleCommentService articleCommentService;
 
     ArticleCommentControllerTest(
             @Autowired MockMvc mvc,
@@ -53,12 +54,10 @@ class ArticleCommentControllerTest {
         willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
 
         // When & Then
-        mvc.perform(
-                        post("/comments/new")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                .content(formDataEncoder.encode(request))
-                                .with(csrf())
-                )
+        mvc.perform(post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
@@ -66,8 +65,8 @@ class ArticleCommentControllerTest {
     }
 
     @Test
-    @DisplayName("[view][GET] 댓글 삭제 - 정상 호출")
     @WithUserDetails(value = "unoTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][GET] 댓글 삭제 - 정상 호출")
     void givenArticleCommentIdToDelete_whenRequesting_thenDeletesArticleComment() throws Exception {
         // Given
         long articleId = 1L;
@@ -76,15 +75,33 @@ class ArticleCommentControllerTest {
         willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId, userId);
 
         // When & Then
-        mvc.perform(
-                        post("/comments/" + articleCommentId + "/delete")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                .content(formDataEncoder.encode(Map.of("articleId", articleId)))
-                                .with(csrf())
-                )
+        mvc.perform(post("/comments/" + articleCommentId + "/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(Map.of("articleId", articleId)))
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
+    }
+
+    @Test
+    @WithUserDetails(value = "unoTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+    void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        // When & Then
+        mvc.perform(post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
     }
 }
